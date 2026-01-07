ba7a4d2a1a67d9acf8bc1b94cd5d7b366fd63529
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2026-01-06 20:28:51 -0500

    console app

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+
+DateTime startDate = DateTime.Today;
+DateTime endDate = new DateTime(2026, 12, 25);
+
+double christmasDays = (endDate - startDate).TotalDays;
+
+Console.WriteLine($"There are {Math.Round(christmasDays)} until the next christmas");
+
+
