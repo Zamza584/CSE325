85cd2cd750851831c7f45b98ef66a5ee547d4237
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2026-01-09 17:54:14 -0500

    new commit

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    int x = 4;
+    Console.WriteLine(x);
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
